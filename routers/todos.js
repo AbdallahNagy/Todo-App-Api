dfc68dcca0a2c8f157f6c31d139160305f5afd0c
AbdallahNagy <abdallahnagy773@gmail.com>
2023-03-05 22:29:43 +0200

    try to run redis but it doesn't work

--- routers/todos.js
+++ routers/todos.js
@@ -6,6 +6,11 @@ const { authorizeUser } = require("../userHelpers");
 const Todo = require("../Models/todo");
 const List = require("../Models/list");
 
+const redis = require('redis');
+const client = redis.createClient();
+client.connect();
+console.log("connected? ",client.isOpen); // this is true
+
 //create todo by list id
 todoRouter.post("/:listId", authorizeUser, async (req, res, next) => {
     try {
@@ -41,7 +46,7 @@ todoRouter.post("/", authorizeUser, async (req, res, next) => {
         const todo = await Todo.create({
             title,
             status,
-            user:id
+            user: id,
         });
 
         res.json(todo);
@@ -55,9 +60,15 @@ todoRouter.get("/", authorizeUser, async (req, res, next) => {
     try {
         const { id } = req.params;
         const todos = await Todo.find({ user: id });
+
+        client.setEx("todos", 3600,"hamada");
+        client.get('todos', (error, data) => {
+            if(error) console.log(error);
+            if(data) console.log(data)
+        })
         res.json(todos);
     } catch (err) {
-        res.next(err);
+        next(err);
     }
 });
 
@@ -87,7 +98,16 @@ todoRouter.patch("/:todoId", authorizeUser, async (req, res, next) => {
 todoRouter.delete("/:todoId", authorizeUser, async (req, res, next) => {
     try {
         const { todoId } = req.params;
-        const deletedTodo = await Todo.findByIdAndDelete(todoId);
+
+        const deletedTodo = await Todo.findByIdAndDelete(todoId, { new: true });
+        if (!deletedTodo) throw customError(404, "todo not found");
+        const list = await List.findById(deletedTodo.list);
+        console.log(list);
+
+        const i = list.todos.indexOf(todoId);
+        list.todos.splice(i, 1);
+        list.save();
+
         res.json(deletedTodo);
     } catch (error) {
         next(error);
